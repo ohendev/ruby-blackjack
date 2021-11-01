2af0d1e7ea0a2618d3f270cc1c3ea3cfd2c59af1
Laurent Vigneron <laur.vigneron@gmail.com>
2021-11-01 16:54:27 -0400

    still some bugs in the logic, chips and cards don't reset

--- main.rb
+++ main.rb
@@ -1,42 +1,45 @@
 require './deck/DeckBuilder'
 require './game/Game'
 require './models/Player'
-game = Game.new
-deck = game.deck
-game.new_table
-dealer = Player.new("Dealer", 1000000)
-player = game.player
+
 play_again = true
 while play_again
+  #set up phase
+  game = Game.new
+  deck = game.deck
+  game.new_table
+  dealer = Player.new("Dealer", 1000000)
+  player = game.player
   #bet round portion
   round_bet = game.bet_amount(player)
-  #draw cards portions
-  player_points = game.draw_a_card(deck, player)
-  player_points += game.draw_a_card(deck, player)
+  #draw cards portion
+  player.points = game.draw_a_card(deck, player)
+  player.points += game.draw_a_card(deck, player)
   puts "Dealer to play:"
-  dealer_points = game.draw_a_card(deck, dealer)
-  dealer_points += game.draw_a_card(deck, dealer)
+  dealer.points = game.draw_a_card(deck, dealer)
+  dealer.points += game.draw_a_card(deck, dealer)
   #player hit or not portion
   another_card = true
   while player.points < 21 && another_card
     another_card = game.hit?(player, dealer)
     if another_card
-      player_points += game.draw_a_card(deck, player)
+      player.points += game.draw_a_card(deck, player)
     end
-    player_cards = player.cards
+    #manage ace
     if player.points > 21
-      for card in player_cards
+      for card in player.cards
         if card.ace
-          player_points -= 10
+          player.points -= 10
         end
       end
     end
   end
   #dealer hit or not portion
   while dealer.points < 17 && player.points < 21
-    dealer_cards = dealer.cards
+    dealer.draw_a_card(deck, dealer)
+    #manage ace
     if dealer.points > 21
-      for card in dealer_cards
+      for card in dealer.cards
         if card.ace
           dealer_cards -= 10
         end
@@ -45,9 +48,16 @@ while play_again
   end
   #win condition checks
   puts "========== who has won ? ==========="
-  player.chips += game.win_check(player, dealer, round_bet)
+  gain_or_loss = game.win_check(player, dealer, round_bet)
+  player.chips += gain_or_loss
+  dealer.chips -= gain_or_loss
   play_again = game.play_again?(player)
+  #resetting phase
   player.reset_cards
   player.reset_points
+  dealer.reset_cards
+  dealer.reset_points
+  game = nil
+  deck = nil
 end
 puts "Thank you! bye!"
